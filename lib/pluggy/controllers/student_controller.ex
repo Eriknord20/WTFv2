45eb18477b28def32b04f23b0768c72445391854
Bijan.fatha <bijanfatha@Bijan-fatha.local>
2018-09-13 12:46:53 +0200

    fungerade login med användarnamn och lösen

--- lib/pluggy/controllers/student_controller.ex
+++ lib/pluggy/controllers/student_controller.ex
@@ -10,7 +10,19 @@ defmodule Pluggy.StudentController do
   end
 
   def test(conn), do: send_resp(conn, 200, render("students/test", []))
-  def index(conn), do: send_resp(conn, 200, render("students/index", []))
+
+  def index(conn) do
+    session_user = conn.private.plug_session["user_id"]
+
+    current_user =
+      case session_user do
+        nil -> nil
+        _ -> User.get(session_user)
+      end
+
+    send_resp(conn, 200, render("students/index", user: current_user))
+  end
+
   def show(conn, id), do: send_resp(conn, 200, render("students/show", student: Student.get(id)))
   # IEx.pry
   def login(conn, params) do
